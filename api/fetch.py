48ca3cfc39b4aad621ef9f10c34817bdb0c3a350
Jan Timpe <jantimpe@email.uark.edu>
2017-06-08 13:57:38 -0500

    Refactoring api methods

--- api/fetch.py
+++ api/fetch.py
@@ -1,43 +1,60 @@
 from database.development import db
 import requests, ijson, gzip, urllib
 
-def read_local_file(filename):
-	file = open(filename, 'r')
-	objects = ijson.items(file, 'CVE_Items.item')
+#
 
-	# cities = (o for o in objects if o['type'] == 'city') # this will be useful i'm sure
-	n = 100
-	chunk = []
-	for item in objects:
-		chunk.append(item)
-
-		if len(chunk) >= n:
-			insert_cve_chunk(chunk)
-			del chunk[:]
-
-	if len(chunk) > 0:
-		insert_cve_chunk(chunk)
-
-def fetch_recent():
+def download_recent():
 	zipped = urllib.request.urlopen('https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.gz')
 	file = gzip.open(zipped)
+
+	return file
+
+def fetch_many(args)
+	result = db.cve_items.find(args)
+
+	return result
+
+def fetch_one(args):
+	result = db.cve_items.find_one(args)
+
+	return result
+
+def insert_many_cves(items)
+	result = db.cve_items.insert_many(items)
+
+	return result
+
+def insert_one_cve(item)
+	result = db.cve_items.insert_one(item)
+
+	return result
+
+def parse_cve_json(file):
 	objects = ijson.items(file, 'CVE_Items.item')
 
-	n = 100
+	return objects
+
+#
+
+def insert_by_chunk(obj_generator, size = 100):
 	chunk = []
-	for item in objects:
+	for item in obj_generator:
 		chunk.append(item)
 
-		if len(chunk) >= n:
-			insert_cve_chunk(chunk)
+		if len(chunk) >= size:
+			insert_many_cves(chunk)
 			del chunk[:]
 
-	if len(chunk) > 0:
-		insert_cve_chunk(chunk)
+	if len(chunk) > 0
+		insert_many_cves(chunk)
 
-def insert_cve_chunk(chunk):
-	new_result = db.cve_items.insert_many(chunk)
+def download_and_insert_recent():
+	file = download_recent()
+	objects = parse_cve_json(file)
+	insert_by_chunk(objects)
 
-def retrieve():
-	result = db.cve_items.find_one({"CVE_data_meta": {"CVE_ID": "CVE-2014-0097"}})
-	return result
+def open_local_and_insert(filename):
+	file = open(filename, 'r')
+	objects = parse_cve_json(file)
+	insert_by_chunk(objects)
+
