ce20d57274c72fab7411d62786aeda73303271d4
Jan Timpe <jantimpe@email.uark.edu>
2017-06-26 08:58:00 -0500

    Search by date

--- main.py
+++ main.py
@@ -1,52 +1,21 @@
 import api.vulnerability_database as api
 from datetime import datetime
 
-print('Starting')
+def print_cve_item(item):
+	cve_id = item['CVE_data_meta']['CVE_id']
+	descriptions = item['CVE_description']['CVE_description_data']
+	references = item['CVE_references']['CVE_reference_data']
 
-api.update_recent()
-print('Downloaded recent')
+# api.refresh_all()
 
-# api.update_modified()
-# print('Downloaded modified')
+results = [
+	api.fetch.by_date(datetime(2017, 6, 20, 0, 0, 0, 0)),
+	api.fetch.by_year(2017),
+	api.fetch.description_contains('e')
+]
 
-# api.update_year(2017)
-# print('Downloaded 2017')
-
-# api.update_year(2015)
-# print('Downloaded 2015')
-
-
-print('Searching for MongoDB vulns')
-# get fancy with regex to do string searching
-# result = api.fetch.many({
-# 	'CVE_description.CVE_description_data': {
-# 		'$elemMatch': {
-# 			'value': {
-# 				'$regex': '.*e.*',
-# 				'$options': 'i'
-# 			}
-# 		}
-# 	}
-# })
-
-result = api.fetch.by_date(datetime(2017, 6, 20, 0, 0, 0, 0))
-
-if result:
-	print(result.count(), 'found')
-	print('====')
-
-	for item in result:
-		descriptions = item['CVE_description']['CVE_description_data']
-		references = item['CVE_references']['CVE_reference_data']
-		print(item['CVE_data_meta']['CVE_ID'])
-
-		for desc in descriptions:
-			print(desc['value'])
-
-		for ref in references:
-			if 'publish_date' in ref:
-				print(ref['publish_date'])
-
-		print('====')
-else:
-	print('Nothing found')
+for result in results:
+	if result:
+		print(result.count(), 'found')
+	else:
+		print('Nothing found')
