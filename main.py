534f09f6d12bb257e8e5e2984e9a4d99599a0eb6
Jan Timpe <jantimpe@email.uark.edu>
2017-07-07 07:34:29 -0500

    Added cvss2 parsing method

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from itertools import chain
 
 def print_cve_item(item):
 	cve_id = item['CVE_data_meta']['CVE_id']
+
 	descriptions = item['CVE_description']['CVE_description_data']
+
 	references = item['CVE_references']['CVE_reference_data']
+	publish_date = references[0]['publish_date']
+	last_update = references[-1]['publish_date']
+
+	cpe = item['CVE_configurations']['CVE_configuration_data']['cpe']
+
+	cve_impact = item['CVE_impact']
+	cve_impact_cvssv2 = cve_impact['CVE_impact_cvssv2']
+	cve_impact_cvssv3 = cve_impact['CVE_impact_cvssv3']
+
+	cwe_id = item['CVE_problemtype']['CVE_problemtype_data'][0]['description']['value']
 
 # api.refresh_all()
 
@@ -20,3 +32,14 @@ for result in results:
 		print(result.count(), 'found')
 	else:
 		print('Nothing found')
+
+
+
+# Cpe string 1,
+# - CVE_ID,
+# - CVE_impact_cvssv3,
+# - CVE_impact_cvssv2,
+# - CWE_ID,
+# - publish time,
+# - cpe,
+# - vulnerability description
