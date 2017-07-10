976192df0aa9be87dc839e6b5aa0cc8a527a66a4
Jan Timpe <jantimpe@email.uark.edu>
2017-07-10 16:54:27 -0500

    Needs rebase

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ from api.models import VulnerabilityVector
 import api.vulnerability_database as api
 import sys, getopt
 
+def die_with_usage_help():
+	print('usage: main.py -i <input> -o <output>')
+
+def get_startup_options(args):
+	# opt_list # do something with this
+
+def main(args):
+	input_file = None
+	output_file = None
+
+	try:
+		opts, args = getopt.getopt(argv,"rhi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
 api.refresh_all()
 
 results = [
